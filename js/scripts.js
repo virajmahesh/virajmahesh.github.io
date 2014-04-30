815011794bd2ec032987d52d6100ca3acdc96916
Viraj Mahesh <virajmahesh@gmail.com>
2014-04-30 13:45:05 -0700

    Made objects static

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,64 @@
 // Executed when page loads
 $(document).ready(function() {
 	var menu = new Menu(); // Creating a new menu
+	var content = new Content(); // Create a new content
 });
 
-Menu = function() {
+function Menu() {
 	// Get a handle to elements
-	this.menu = $("#menu");
-	this.content = $("#content-container");
-	this.toggle = $("#menu-toggle");
-	this.menuItems = $(".menu-text");
+	Menu.menu = $("#menu");
+	Menu.toggle = $("#menu-toggle");
+	Menu.menuItems = $(".menu-text");
 
 	// Setting the menu properties
-	this.collapsedWidth = "60px";
-	this.expandedWidth = "225px";
-	this.hidden = true;
+	Menu.collapsedWidth = "60px";
+	Menu.expandedWidth = "225px";
+	Menu.hidden = true;
 
 	// The menu is initially collapsed
-	this.menu.width(this.collapsedWidth);
-	this.content.css('left', this.collapsedWidth);
-	this.setCallback();
-	this.menuItems.hide();
+	Menu.menu.width(Menu.collapsedWidth);
+	Menu.setCallback();
+	Menu.menuItems.hide();
 }
 
-Menu.prototype.collapse = function() {
-	this.menu.animate({width: this.collapsedWidth});
-	this.menuItems.hide();
+Menu.collapse = function() {
+	Menu.menu.animate({width: Menu.collapsedWidth});
+	Menu.menuItems.hide();
 }
 
-Menu.prototype.expand = function() {
-	this.menu.animate({width: this.expandedWidth});
-	this.menuItems.show();
+Menu.expand = function() {
+	Menu.menu.animate({width: Menu.expandedWidth});
+	Menu.menuItems.show();
 }
 
-Menu.prototype.setCallback = function() {
-	var that = this;
-	this.toggle.click(function() {
-		if (that.hidden) {
-			that.expand();
-			that.hidden = false;
-		}
-		else if (!that.hidden) {
-			that.collapse();
-			that.hidden = true;
+Menu.setCallback = function() {
+	Menu.toggle.mouseover(function() {
+		if (Menu.hidden) {
+			Menu.expand();
+			Menu.hidden = false;
+			Content.dim();
 		}
 	});
+	Menu.menu.mouseleave(function() {
+		if (!Menu.hidden) {
+			Menu.collapse();
+			Menu.hidden = true;
+			Content.light();
+		}
+	});
+}
+
+Content = function() {
+	Content.contentContainer = $("#content-container");
+	Content.contentOverlay = $("#content-overlay");
+	console.log(Content.contentOverlay);
+}
+
+Content.dim = function() {
+	Content.contentOverlay.fadeTo("fast", opacity=0.6);
+}
+
+Content.light = function() {
+	Content.contentOverlay.fadeTo("fast", opacity=0.0);
 }
 
