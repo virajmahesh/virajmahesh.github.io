ea94b693689a5d1b3bd7d8c62e03a6055970be84
Viraj Mahesh <virajmahesh@gmail.com>
2014-04-30 17:28:54 -0700

    Revert "Revert 9e66119..68d5fe1"
    This reverts commit 2118c2d3f93ecdd0a38609de0162267261851ffc.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,6 @@ $(document).ready(function() {
 function Menu() {
 	// Get a handle to elements
 	Menu.menu = $("#menu");
-	Menu.toggle = $(".menu-image");
-	Menu.menuItems = $(".menu-text");
-	Menu.menuItemContainers = $(".item-container");
 
 	// Setting the menu properties
 	Menu.collapsedWidth = "60px";
@@ -18,53 +15,107 @@ function Menu() {
 
 	// The menu is initially collapsed
 	Menu.menu.width(Menu.collapsedWidth);
-	Menu.setCallbacks();
-	Menu.menuItems.hide();
-}
+	Menu.setCallback();
+	Menu.populateMenuItems();
+};
 
 Menu.collapse = function() {
 	Menu.menu.animate({width: Menu.collapsedWidth});
-	Menu.menuItems.hide();
-}
+	MenuItem.hide(Menu.items);
+	console.trace();
+};
 
 Menu.expand = function() {
+	console.trace();
 	Menu.menu.animate({width: Menu.expandedWidth});
-	Menu.menuItems.show();
-}
+	MenuItem.show(Menu.items);
+};
 
-Menu.setCallbacks = function() {
+Menu.setCallback = function() {
 	Menu.menu.mouseleave(function() {
 		if (!Menu.hidden) {
 			Menu.collapse();
 			Menu.hidden = true;
 			Content.light();
+			MenuItem.hide(Menu.items);
 		}
 	});
+};
 
-	Menu.menuItemContainers.mouseover(function() {
+Menu.populateMenuItems = function() {
+	var allItems = $(".item-container");
+	Menu.items = new Array();
+	allItems.toArray().forEach(function(elem, index, array) {
+		Menu.items.push(new MenuItem(elem)); // Create a new MenuItem out of the element
+	});
+};
+
+// Represents the page content
+Content = function() {
+	Content.contentContainer = $("#content-container");
+	Content.contentOverlay = $("#content-overlay");
+};
+
+Content.dim = function() {
+	Content.contentOverlay.fadeTo("fast", opacity=0.6);
+};
+
+Content.light = function() {
+	Content.contentOverlay.fadeTo("fast", opacity=0.0);
+};
+
+// Represents a menu item
+MenuItem = function(object) {
+	this.object = $(object);
+	this.allChildren = this.object.children();
+	this.image = this.object.children(".menu-image");
+	this.text = this.object.children(".menu-text");
+
+	this.setCallback();
+};
+
+MenuItem.prototype.darkenBackground = function() {
+	this.object.css("background-color", "rgb(29, 29, 50)");
+};
+
+MenuItem.prototype.setCallback = function() {
+	var that = this;
+	this.object.mouseover(function() {
+		console.log(this);
 		if (Menu.hidden) {
 			Menu.expand();
 			Menu.hidden = false;
 			Content.dim();
 		}
-		$(this).css("background-color", "rgb(29, 29, 50)");
+		that.darkenBackground();
 	});
 
-	Menu.menuItemContainers.mouseleave(function() {
-		$(this).css("background-color", "rgb(51, 51, 102)");
+	this.object.mouseleave(function() {
+		that.object.css("background-color", "rgb(51, 51, 102)");
 	});
+};
+
+// Hide an entire list of MenuItems
+MenuItem.hide = function(itemList) {
+	itemList.forEach(function(elem, index, array) {
+		elem.hide();
+	});
+};
+
+// Hide this object
+MenuItem.prototype.hide = function() {
+	this.text.hide();
 }
 
-Content = function() {
-	Content.contentContainer = $("#content-container");
-	Content.contentOverlay = $("#content-overlay");
-}
+// Show an entire list of MenuItems
+MenuItem.show = function(itemList) {
+	itemList.forEach(function(elem, index, array) {
+		elem.show();
+	});
+};
 
-Content.dim = function() {
-	Content.contentOverlay.fadeTo("fast", opacity=0.6);
-}
-
-Content.light = function() {
-	Content.contentOverlay.fadeTo("fast", opacity=0.0);
+// Show this object
+MenuItem.prototype.show = function() {
+	this.text.show();
 }
 
